e6de532a74a5db3eb1582034bd856671ceedd948
ahmedmlaouhia <ahmedmloauhia6@gmail.com>
2024-05-15 22:02:07 +0100

    works

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,13 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.4.0")
+        classpath ("com.android.tools.build:gradle:8.2.2")
         classpath("com.google.gms:google-services:4.4.1")
     }
 }
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.0" apply false
-    id("com.android.library") version "8.4.0" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
 }
